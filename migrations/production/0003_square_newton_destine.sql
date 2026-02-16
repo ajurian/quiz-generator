188f64e2e80f91c3bdab862cb9d704a3a2433d9d
ajurian <adolfjamesurian@gmail.com>
2026-02-16 22:33:45 +0800

    chore(migrations): added drizzle migrations on production environment

---
+++ migrations/production/0003_square_newton_destine.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "quiz_attempt_answers" (
+	"quiz_attempt_id" uuid NOT NULL,
+	"question_id" uuid NOT NULL,
+	"answer" varchar(255) NOT NULL,
+	CONSTRAINT "quiz_attempt_answers_quiz_attempt_id_question_id_pk" PRIMARY KEY("quiz_attempt_id","question_id")
+);
+--> statement-breakpoint
+ALTER TABLE "quiz_attempts" ADD COLUMN "parent_attempt_id" uuid;--> statement-breakpoint
+ALTER TABLE "quiz_attempts" ADD COLUMN "locked_question_ids" jsonb DEFAULT '[]'::jsonb NOT NULL;--> statement-breakpoint
+ALTER TABLE "quiz_attempt_answers" ADD CONSTRAINT "quiz_attempt_answers_quiz_attempt_id_quiz_attempts_id_fk" FOREIGN KEY ("quiz_attempt_id") REFERENCES "public"."quiz_attempts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiz_attempt_answers" ADD CONSTRAINT "quiz_attempt_answers_question_id_questions_id_fk" FOREIGN KEY ("question_id") REFERENCES "public"."questions"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "quiz_attempt_answers_attempt_idx" ON "quiz_attempt_answers" USING btree ("quiz_attempt_id");--> statement-breakpoint
+ALTER TABLE "quiz_attempts" ADD CONSTRAINT "quiz_attempts_parent_attempt_id_quiz_attempts_id_fk" FOREIGN KEY ("parent_attempt_id") REFERENCES "public"."quiz_attempts"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiz_attempts" DROP COLUMN "answers";
